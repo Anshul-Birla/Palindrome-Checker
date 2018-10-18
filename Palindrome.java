4742fb00d1527dbcc4261703d60ba7564815f32d
Anshul Birla <anshulbirla@gmail.com>
2018-10-18 11:54:47 -0700

    added checks for spaces

--- Palindrome.java
+++ Palindrome.java
@@ -7,18 +7,38 @@
  */
 class Palindrome
 {
-    String input, reversed;
+    private String input, reversed,;
     
     Palindrome(String str)
     {
-        input = str;
-        
+        input = lower(str);
+        reversed = input;
     }
     
-    public String lower()
+    public String lower(String str)
     {
-        reversed = input.toLowerCase();
-        return reversed;
+        return str.toLowerCase();
     }
+    
+    public String reverse(String str)
+    {
+        int strngLength;
+        strngLength = input.length();
+        String temp = input;
+        String finalStr = "";
+        
+        for(x =0; x<strlength;x++)
+        {
+            temp = temp.substring(x, x+1);
+            if ((temp.compareTo("0") >= 0 && temp.compareTo("9") <= 0) || 
+                (temp.compareTo("A") >= 0 && temp.compareTo("Z") <= 0) ||
+                (temp.compareTo("a") >= 0 && temp.compareTo("z") <= 0));
+                {
+                    finalStr = finalStr + temp;
+                }   
+    }
+    
+    
     
 }
+
